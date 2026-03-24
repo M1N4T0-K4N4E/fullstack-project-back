61cc921c3e44d62a8b0e7e1070994e3ff4d724d9
Boby <boblaveyou@gmail.com>
2026-03-24 15:33:43 +0700

    restruct project

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,9 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { serverLogger, userInteractionLogger } from './utils/logger.js'
-import { db } from './db/index.js'
-import { blacklistedTokens } from './db/schema.js'
-import { lt } from 'drizzle-orm'
 import roles from './routes/roles.js'
-import events from './routes/events.js'
+import posts from './routes/posts.js'
 import account from './routes/account.js'
-import tickets from './routes/tickets.js'
 import auth from './routes/auth.js'
 import upload from './routes/upload.js'
 import users from './routes/users.js'
@@ -32,9 +28,8 @@ app.get('/', (c) => {
 
 // Mount API routes
 app.route('/api/roles', roles)
-app.route('/api/events', events)
+app.route('/api/posts', posts)
 app.route('/api/account', account)
-app.route('/api/tickets', tickets)
 app.route('/api/auth', auth)
 app.route('/api/upload', upload)
 app.route('/api/users', users)
@@ -45,13 +40,3 @@ serve({
 }, (info) => {
   serverLogger.info(`Server is running on http://localhost:${info.port}`)
 })
-
-// Background job to clean up expired blacklisted tokens every 12 hours
-setInterval(async () => {
-  try {
-    await db.delete(blacklistedTokens).where(lt(blacklistedTokens.expiresAt, new Date()));
-    serverLogger.info('Cleaned up expired blacklisted tokens scheduling job completed');
-  } catch (error) {
-    serverLogger.error('Failed to clean up blacklisted tokens', { error });
-  }
-}, 1000 * 60 * 60 * 12);
