ac673a84933c94487a05a47adfb2712ebb240430
Boby <boblaveyou@gmail.com>
2026-03-24 15:35:38 +0700

    add redis

---
+++ src/utils/redis.ts
@@ -0,0 +1,17 @@
+import { Redis } from 'ioredis';
+import { serverLogger } from './logger.js';
+
+const redis = new Redis({
+  host: process.env.REDIS_HOST || 'localhost',
+  port: parseInt(process.env.REDIS_PORT || '3002'),
+});
+
+redis.on('connect', () => {
+  serverLogger.info('Redis client connected');
+});
+
+redis.on('error', (err: Error) => {
+  serverLogger.error('Redis client error', { error: err });
+});
+
+export default redis;
